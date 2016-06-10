ae7bdf626de12e0092cf424163497c7ff51396b5
bsautermeister <benjamin.sautermeister@tum.de>
2016-06-10 18:06:40 +0200

    Removed variable (weight, bias) function Due to they provided no flexibility and no code reduction due to the provied functions of TensorFlow.

--- network.py
+++ network.py
@@ -228,29 +228,3 @@ def corrupt(x):
                                                maxval=2,
                                                dtype=tf.int32), tf.float32))
 
-
-# DEPRECATED: Offers low flexibility. Use tf.get_variable() instead.
-def weight_variable(shape):
-    '''Helper function to create a weight variable initialized with
-    a normal distribution.
-    Parameters
-    ----------
-    shape : list
-        Size of weight variable
-    '''
-    initial = tf.random_normal(shape, mean=0.0, stddev=0.01)
-    return tf.Variable(initial)
-
-
-# DEPRECATED: Offers low flexibility. Use tf.get_variable() instead.
-def bias_variable(shape):
-    '''Helper function to create a bias variable initialized with
-    a constant value.
-    Parameters
-    ----------
-    shape : list
-        Size of weight variable
-    '''
-    initial = tf.constant(shape, mean=0.0, stddev=0.01)
-    return tf.Variable(initial)
-
