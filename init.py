2ed0131d27bf6576e8a88d42b95b0f012d79a8c4
bsautermeister <benjamin.sautermeister@tum.de>
2016-07-12 17:14:53 +0200

    Fixed wrong line indent

--- init.py
+++ init.py
@@ -52,11 +52,11 @@ def orthogonal_initializer(scale = 1.1):
         Returns the init function.
     """
     def _initializer(shape, dtype=tf.float32):
-      flat_shape = (shape[0], np.prod(shape[1:]))
-      a = np.random.normal(0.0, 1.0, flat_shape)
-      u, _, v = np.linalg.svd(a, full_matrices=False)
-      # pick the one with the correct shape
-      q = u if u.shape == flat_shape else v
-      q = q.reshape(shape) #this needs to be corrected to float32
-      return tf.constant(scale * q[:shape[0], :shape[1]], dtype=tf.float32)
+        flat_shape = (shape[0], np.prod(shape[1:]))
+        a = np.random.normal(0.0, 1.0, flat_shape)
+        u, _, v = np.linalg.svd(a, full_matrices=False)
+        # pick the one with the correct shape
+        q = u if u.shape == flat_shape else v
+        q = q.reshape(shape) #this needs to be corrected to float32
+        return tf.constant(scale * q[:shape[0], :shape[1]], dtype=tf.float32)
     return _initializer
