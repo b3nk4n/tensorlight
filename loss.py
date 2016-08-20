b78e1847af66bdcbc40e63f124b52a6a9f94f74b
bsautermeister <benjamin.sautermeister@tum.de>
2016-08-20 23:42:32 +0200

    Optimized code to skip unnecessary computation steps in (MS-)SSIM

--- loss.py
+++ loss.py
@@ -97,7 +97,7 @@ def _fspecial_gauss(size, sigma):
     return g / tf.reduce_sum(g)
 
 
-def ssim(img1, img2, patch_size=11, sigma=1.5, cs_map=False):
+def ssim(img1, img2, patch_size=11, sigma=1.5, L=255, K1=0.01, K2=0.03, cs_map=False):
     """Calculates the Structural Similarity Metric corresponding to input images
        Reference: 
            This function attempts to mimic precisely the functionality of ssim.m a
@@ -113,8 +113,18 @@ def ssim(img1, img2, patch_size=11, sigma=1.5, cs_map=False):
         The size of a single patch.
     sigma: float, optional
         The Gaussian's sigma value.
+    L: int, optional
+        Note: Not using 255 will result in slightly different result.
+        The bit depth of the image. Use '1' when a value scale of [0,1] is used.
+        The scale of [-1, 1] is not supported and has to be rescaled.
+    K1: float, optional
+        The K1 value.
+    K2: float, optional
+        The K2 value.
     cs_map: Boolean, optional
-        Whether to return the cs mapping as well. Basically only used internally.
+        Whether to return the constrast-structure product only,
+        instead of the complete SSIM.
+        Basically only used internally for performance. Do not use it from the outside.
     Returns
     ----------
     value: float32
@@ -122,10 +132,6 @@ def ssim(img1, img2, patch_size=11, sigma=1.5, cs_map=False):
         they are identical and '0' means they are completely different.
     """
     window = _fspecial_gauss(patch_size, sigma)
-    K1 = 0.01
-    K2 = 0.03
-    #L = 255 #bitdepth of image
-    L = 1
     C1 = (K1*L)**2
     C2 = (K2*L)**2
     mu1 = tf.nn.conv2d(img1, window, strides=[1,1,1,1], padding='VALID')
@@ -137,16 +143,17 @@ def ssim(img1, img2, patch_size=11, sigma=1.5, cs_map=False):
     sigma2_sq = tf.nn.conv2d(img2*img2, window, strides=[1,1,1,1], padding='VALID') - mu2_sq
     sigma12 = tf.nn.conv2d(img1*img2, window, strides=[1,1,1,1], padding='VALID') - mu1_mu2
     
-    ssim_value = tf.reduce_mean(((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*
-                                (sigma1_sq + sigma2_sq + C2)))
+    l_p = (2.0 * mu1_mu2 + C1) / (mu1_sq + mu2_sq + C1)
+    cs_p = (2.0 * sigma12 + C2) / (sigma1_sq + sigma2_sq + C2)
+   
     if cs_map:
-        cs_map_value = tf.reduce_mean((2.0*sigma12 + C2)/(sigma1_sq + sigma2_sq + C2))
-        return ssim_value, cs_map_value
-    else:
-        return ssim_value
+        return tf.reduce_mean(cs_p)
+   
+    ssim_value =  tf.reduce_mean(l_p * cs_p)
+    return ssim_value
 
 
-def ms_ssim(img1, img2, patch_size=11, sigma=1.5, level=5):
+def ms_ssim(img1, img2, patch_size=11, sigma=1.5, L=255, K1=0.01, K2=0.03, levels=5):
     """Calculates the Multi-Scale Structural Similarity (MS-SSIM) Image
        Quality Assessment according to Z. Wang.
        References:
@@ -166,7 +173,15 @@ def ms_ssim(img1, img2, patch_size=11, sigma=1.5, level=5):
         The size of a single patch.
     sigma: float, optional
         The Gaussian's sigma value.
-    level: int, optional
+    L: int, optional
+        WARNING: NOT USING 255 WILL RESULT IN DIFFERENT RESULTS!
+        The bit depth of the image. Use '1' when a value scale of [0,1] is used.
+        The scale of [-1, 1] is not supported and has to be rescaled.
+    K1: float, optional
+        The K1 value.
+    K2: float, optional
+        The K2 value.
+    levels: int, optional
         The number of scale levels. Must be in range [2, 5].
         We do not allow level=1, because then ssid() should be used for efficiency.
         We do not allow level>5, because empirical weights higher levels are missing.
@@ -176,23 +191,26 @@ def ms_ssim(img1, img2, patch_size=11, sigma=1.5, level=5):
         The multi-scale structural similarity metric value between both images,
         where '1' means they are identical and '0' means they are completely different.
     """
-    assert level >= 2 and level <= 5, "Level must be in range [2, 5]."
+    assert levels >= 2 and levels <= 5, "Levels must be in range [2, 5]."
     
     weight = tf.constant([0.0448, 0.2856, 0.3001, 0.2363, 0.1333], dtype=tf.float32)
-    mssim = []
+    mssim = None
     mcs = []
-    for l in xrange(level):
-        ssim_map, cs_map = ssim(img1, img2, patch_size, sigma, cs_map=True)
-        mssim.append(ssim_map)
-        mcs.append(cs_map)
+    for l in xrange(levels):
+        if l == levels - 1:
+            mssim = ssim(img1, img2, patch_size, sigma, L, K1, K2, cs_map=False)
+        else:
+            cs_map = ssim(img1, img2, patch_size, sigma, cs_map=True)
+            mcs.append(cs_map)
+        
+        # ndimage.filters.convolve(img, downsample_filter, mode='reflect')
         img1 = tf.nn.avg_pool(img1, [1,2,2,1], [1,2,2,1], padding='SAME')
         img2 = tf.nn.avg_pool(img2, [1,2,2,1], [1,2,2,1], padding='SAME')
 
     # list to tensor of dim D+1
-    mssim = tf.pack(mssim, axis=0)
     mcs = tf.pack(mcs, axis=0)
 
-    value = (tf.reduce_prod(mcs[0:level-1]**weight[0:level-1])*
-                            (mssim[level-1]**weight[level-1]))
+    value = (tf.reduce_prod(mcs**weight[0:levels-1])*
+                            (mssim**weight[levels-1]))
 
     return tf.reduce_mean(value)
