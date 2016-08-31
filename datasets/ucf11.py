6aac88528511cde282b078622c8d98fe8a43e7f9
bsautermeister <benjamin.sautermeister@tum.de>
2016-08-31 15:06:09 +0200

    Implemented detection for changed image properties
    For UCF11 dataset. When flattened shape has changed, delete all *.seq files and re-serialize file when dataset is created.
    Also added new parameters to constructor.

--- datasets/ucf11.py
+++ datasets/ucf11.py
@@ -8,27 +8,30 @@ import base
 
 
 UCF11_URL = 'http://crcv.ucf.edu/data/UCF11_updated_mpg.rar'
-SERIALIZED_SEQ_LENGTH = 30
-MIN_FRACTION_EXAMPLES_IN_QUEUE = 0.1
 
 FRAME_HEIGHT = 240
-FRAME_WIDTH = 320  # TODO: remove these constants???
+FRAME_WIDTH = 320
 FRAME_CHANNELS = 3
-
-DO_DISTORTION = False # Activating this is super slow! (factor 8-10)
-
 
 class UCF11TrainDataset(base.AbstractImageSequenceDataset):
     """UCF-11 sports dataset that creates a bunch of binary frame sequences
        and uses a file queue for multi-threaded input reading.
     """
     def __init__(self, input_seq_length=10, target_seq_length=10,
-                 image_size=(FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNELS)):
+                 image_size=(FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNELS),
+                 serialized_sequence_length=30, min_exampled_in_queue=256,
+                 num_threads=8, do_distortion=True):
         """Creates a dataset instance.
         Parameters
         ----------
         ... TODO: describe parameters of this classes.
+        do_distortion: factor >8x slower for long frame sequences
         """
+        self._serialized_sequence_length = serialized_sequence_length
+        self._min_examples_in_queue = min_exampled_in_queue
+        self._num_threads = num_threads
+        self._do_distortion = do_distortion
+        
         try:
             rar_path = tt.utils.data.download(UCF11_URL, 'tmp')
             dataset_path = tt.utils.data.extract(rar_path, 'tmp')
@@ -38,20 +41,33 @@ class UCF11TrainDataset(base.AbstractImageSequenceDataset):
             print 'Please set the correct path to UCF11 dataset. Might be caused by a download error.'
             sys.exit()
         
-        dataset_size = self._serialize_frame_sequences(dataset_path, image_size)
+        dataset_size = UCF11TrainDataset._serialize_frame_sequences(dataset_path,
+                                                                    image_size,
+                                                                    serialized_sequence_length)
         data = None
         
         super(UCF11TrainDataset, self).__init__(data, dataset_size, image_size,
                                                      input_seq_length, target_seq_length)
 
     @staticmethod
-    def _serialize_frame_sequences(dataset_path, image_size):
+    def _serialize_frame_sequences(dataset_path, image_size, serialized_sequence_length):
         sequence_files = tt.utils.path.get_filenames(dataset_path, '*.seq')
         
         if len(sequence_files) > 0:
-            dataset_size = len(sequence_files)
-            print("Found {} serialized frame sequences. Skipping serialization.". format(dataset_size))
-            return dataset_size
+            # Test if image_size has changed
+            example = np.fromfile(sequence_files[0], np.uint8)
+            old_ndim = np.prod(example.shape)
+            new_ndim = np.prod(image_size) * serialized_sequence_length
+            if old_ndim == new_ndim:
+                # Reuse old serialized files
+                dataset_size = len(sequence_files)
+                print("Found {} serialized frame sequences. Skipping serialization.".format(dataset_size))
+                return dataset_size
+            else:
+                print("Change in image properties detected. Deleting previous serialized *.seq files...")
+                # remove old serialized files
+                for sfile in sequence_files:
+                    os.remove(sfile)            
 
         video_filenames = tt.utils.path.get_filenames(dataset_path, '*.mpg')
         frame_scale_factor = image_size[0] / float(FRAME_HEIGHT)
@@ -59,7 +75,7 @@ class UCF11TrainDataset(base.AbstractImageSequenceDataset):
         for i, video_filename in enumerate(video_filenames):
             with tt.utils.video.VideoReader(video_filename) as vr:
                 frames = []
-                for f in xrange(SERIALIZED_SEQ_LENGTH):
+                for f in xrange(serialized_sequence_length):
                     frame = vr.next_frame(frame_scale_factor)
                     if frame is not None:
                         # ensure frame is not too large
@@ -67,7 +83,7 @@ class UCF11TrainDataset(base.AbstractImageSequenceDataset):
                         if h > image_size[0] or w > image_size[1]:
                             frame = frame[:image_size[0], :image_size[1], :]
                         if not h < image_size[0] and not w < image_size[1]:
-                            frame = np.reshape(frame, [image_size[0], image_size[1], -1]) # TODO: no -1 here necessary
+                            frame = np.reshape(frame, [image_size[0], image_size[1], -1])
                             if image_size[2] == 1:
                                 frame = tt.utils.image.to_grayscale(frame)
                             frames.append(frame)
@@ -78,7 +94,7 @@ class UCF11TrainDataset(base.AbstractImageSequenceDataset):
                         print('Warning: Frame sequence too short. Skipping.')
                         break
 
-                if len(frames) == SERIALIZED_SEQ_LENGTH:
+                if len(frames) == serialized_sequence_length:
                     # TODO: seqences from one folder to a single file?
                     seq_filepath = os.path.splitext(video_filename)[0] + '.seq'
                     tt.utils.image.write_as_binary(seq_filepath, np.asarray(frames))
@@ -99,7 +115,7 @@ class UCF11TrainDataset(base.AbstractImageSequenceDataset):
         frame_bytes = record.height * record.width * record.depth
         total_seq_length = self.input_seq_length + self.target_seq_length
         record_bytes = frame_bytes * (self.input_seq_length + self.target_seq_length)
-        total_file_bytes = frame_bytes * SERIALIZED_SEQ_LENGTH
+        total_file_bytes = frame_bytes * self._serialized_sequence_length
 
         reader = tf.FixedLengthRecordReader(total_file_bytes)
 
@@ -109,10 +125,11 @@ class UCF11TrainDataset(base.AbstractImageSequenceDataset):
         record.data = decoded_record_bytes[0:self.input_seq_length]
 
         decoded_record_bytes = tf.reshape(decoded_record_bytes,
-                                          [SERIALIZED_SEQ_LENGTH, record.height, record.width, record.depth])
+                                          [self._serialized_sequence_length, record.height, record.width, record.depth])
 
-        # calculcate tensors [start, 0, 0, 0] and [start + INPUT_SEQ_LENGTH, 0, 0, 0]
-        rnd_start_index = tf.to_int32(tf.random_uniform([1], 0, SERIALIZED_SEQ_LENGTH - (total_seq_length), tf.int32))
+        # calculcate tensors [start, 0, 0, 0]
+        rnd_start_index = tf.to_int32(tf.random_uniform([1], 0, self._serialized_sequence_length - (total_seq_length), 
+                                                        tf.int32))
         seq_start_offset = tf.SparseTensor(indices=[[0]], values=rnd_start_index, shape=[4])
         sequence_start = tf.sparse_tensor_to_dense(seq_start_offset)
 
@@ -128,7 +145,7 @@ class UCF11TrainDataset(base.AbstractImageSequenceDataset):
             data_dir: Path to the data directory.
             batch_size: Number of image sequences per batch.
         Returns:
-            images: Images. 4D tensor of [batch_size, FRAME_HEIGHT, FRAME_WIDTH, 3 * INPUT_SEQ_LENGTH] size.
+            images: Images. 4D tensor of [batch_size, FRAME_HEIGHT, FRAME_WIDTH, 3] size.
         """
         seq_filenames = tt.utils.path.get_filenames(self._data_dir, '*.seq')
         with tf.name_scope('preprocessing'):
@@ -139,7 +156,7 @@ class UCF11TrainDataset(base.AbstractImageSequenceDataset):
             reshaped_seq = (reshaped_seq - 127.5) / 127.5
     
             # distort images
-            if DO_DISTORTION:
+            if self._do_distortion:
                 with tf.name_scope('distort_inputs'):
                     images_to_distort = []
                     for i in xrange(self.input_seq_length + self.target_seq_length):
@@ -152,11 +169,8 @@ class UCF11TrainDataset(base.AbstractImageSequenceDataset):
                 stacked_distorted_inputs = reshaped_seq[0:self.input_seq_length,:,:,:]
                 stacked_distorted_targets = reshaped_seq[self.input_seq_length:,:,:,:]
 
-        # Ensure that the random shuffling has good mixing properties.
-        min_queue_examples = int(self.size * MIN_FRACTION_EXAMPLES_IN_QUEUE)
-
         # Generate a batch of sequences and labels by building up a queue of examples.
         batch = tt.inputs.generate_batch(stacked_distorted_inputs, stacked_distorted_targets,
-                                        batch_size, min_queue_examples, 
-                                        shuffle=True, num_threads=8)
+                                        batch_size, self._min_examples_in_queue, 
+                                        shuffle=True, num_threads=self._num_threads)
         return batch
