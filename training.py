28a6280c677e5bd11a4c32ec0e8a20a551fd75b4
bsautermeister <benjamin.sautermeister@tum.de>
2016-09-17 00:30:05 +0200

    Fixed bugs in inverse sigmoid decay

--- training.py
+++ training.py
@@ -221,7 +221,7 @@ def average_gradients(tower_grads):
         return average_grads
     
 
-def inverse_sigmoid_decay(decay_variable, global_step, decay_rate=1000.0,
+def inverse_sigmoid_decay(initial_value, global_step, decay_rate=1000.0,
                           name=None):
     """Applies inverse sigmoid decay to the decay variable (learning rate).
     When training a model, it is often recommended to lower the learning rate as
@@ -248,7 +248,7 @@ def inverse_sigmoid_decay(decay_variable, global_step, decay_rate=1000.0,
     
     Parameters
     ----------
-    decay_variable: A scalar `float32` or `float64` `Tensor` or a
+    initial_value: A scalar `float32` or `float64` `Tensor` or a
       Python number.  The initial variable value to decay.
     global_step: A scalar `int32` or `int64` `Tensor` or a Python number.
       Global step to use for the decay computation.  Must not be negative.
@@ -265,14 +265,12 @@ def inverse_sigmoid_decay(decay_variable, global_step, decay_rate=1000.0,
     """
     assert decay_rate > 1, "The decay_rate has to be >> 1."
     
-    with ops.name_scope(name, "InvSigmoidDecay",
-                        [decay_variable, global_step,
-                         decay_steps, decay_rate]) as name:
-        decay_variable = ops.convert_to_tensor(decay_variable, name="decay_variable")
-        dtype = decay_variable.dtype
-        global_step = math_ops.cast(global_step, dtype)
-        decay_steps = math_ops.cast(decay_steps, dtype)
-        decay_rate = math_ops.cast(decay_rate, dtype)
+    with tf.op_scope([initial_value, global_step, decay_rate],
+                    name, "InvSigmoidDecay") as name:
+        initial_value = tf.convert_to_tensor(initial_value, name="initial_value")
+        dtype = initial_value.dtype
+        global_step = tf.cast(global_step, dtype)
+        decay_rate = tf.cast(decay_rate, dtype)
         
         denom = decay_rate + tf.exp(global_step / decay_rate)
-        return math_ops.mul(decay_variable, decay_rate / denom, name=name)
+        return tf.mul(initial_value, decay_rate / denom, name=name)
