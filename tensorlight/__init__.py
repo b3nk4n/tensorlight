79bb79a398d50e80911b07b26f0a7331789a62f6
bsautermeister <benjamin@comfylight.com>
2017-05-05 14:55:04 +0200

    Moved file to subdir to be structured for setup.py

---
+++ tensorlight/__init__.py
@@ -0,0 +1,17 @@
+# import utils first, becuase of its attribute definitions
+import tensorlight.utils
+
+import tensorlight.board
+import tensorlight.core
+import tensorlight.datasets
+import tensorlight.hardware
+import tensorlight.image
+import tensorlight.init
+import tensorlight.inputs
+import tensorlight.loss
+import tensorlight.mathex
+import tensorlight.model
+import tensorlight.network
+# import tensorlight.recurrent
+import tensorlight.training
+import tensorlight.visualization
