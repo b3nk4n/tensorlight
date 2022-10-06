d24a806f2833356300fa4ea002b366ec45316a67
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 21:53:56 +0000

    Adding tarfile member sanitization to extractall()

--- tensorlight/utils/data.py
+++ tensorlight/utils/data.py
@@ -84,7 +84,26 @@ def extract(filepath, target_dir, unpacked_name=None):
                 rar.extractall(target_dir)
         elif filename.endswith(EXT_TAR_GZ):
             with tarfile.open(filepath, 'r:gz') as tar:
-                tar.extractall(target_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, target_dir)
         elif filename.endswith(EXT_ZIP):
             with zipfile.ZipFile(filepath, 'r') as zfile:
                 zfile.extractall(target_dir)
